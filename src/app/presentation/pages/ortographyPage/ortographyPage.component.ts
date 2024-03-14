1ec19a3f9aa038303c8a6cba93c50a9896f77b90
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2024-03-14 17:13:16 +0100

    Se implementa el caso de uso de obtener corrección de ortografía de un texto

--- src/app/presentation/pages/ortographyPage/ortographyPage.component.ts
+++ src/app/presentation/pages/ortographyPage/ortographyPage.component.ts
@@ -6,16 +6,11 @@ import {
   signal,
 } from '@angular/core';
 import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
+import { GptMessageOrtographyComponent } from '@components/chat-bubbles/gptMessageOrtography/gptMessageOrtography.component';
 import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
 import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
-import {
-  TextMessageBoxFileComponent,
-  TextMessageEvent,
-} from '@components/text-boxes/textMessageBoxFile/textMessageBoxFile.component';
-import {
-  TextMessageBoxEvent,
-  TextMessageBoxSelectComponent,
-} from '@components/text-boxes/textMessageBoxSelect/textMessageBoxSelect.component';
+import { TextMessageBoxFileComponent } from '@components/text-boxes/textMessageBoxFile/textMessageBoxFile.component';
+import { TextMessageBoxSelectComponent } from '@components/text-boxes/textMessageBoxSelect/textMessageBoxSelect.component';
 import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
 import { Message } from '@interfaces/message.interface';
 import { OpenAiService } from 'app/presentation/services/openai.service';
@@ -31,24 +26,26 @@ import { OpenAiService } from 'app/presentation/services/openai.service';
     TextMessageBoxComponent,
     TextMessageBoxFileComponent,
     TextMessageBoxSelectComponent,
+    GptMessageOrtographyComponent,
   ],
   templateUrl: './ortographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrtographyPageComponent {
-  public messages = signal<Message[]>([{ text: 'Hola mundo', isGpt: false }]);
+  public messages = signal<Message[]>([]);
   public isLoading = signal<boolean>(false);
   public openAiService = inject(OpenAiService);
 
-  handleMessage(message: string): void {
-    console.log({ message });
-  }
+  handleMessage(prompt: string): void {
+    this.isLoading.set(true);
+    this.messages.update((prev) => [...prev, { text: prompt, isGpt: false }]);
 
-  handleMessageFile({ prompt, file }: TextMessageEvent): void {
-    console.log({ prompt, file });
-  }
-
-  handleMessageSelect({ prompt, selectedOption }: TextMessageBoxEvent): void {
-    console.log({ prompt, selectedOption });
+    this.openAiService.checkOrtography(prompt).subscribe((response) => {
+      this.isLoading.set(false);
+      this.messages.update((prev) => [
+        ...prev,
+        { text: response.message, isGpt: true, info: response },
+      ]);
+    });
   }
 }
--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { ortographyUseCase } from '@use-cases/ortography/ortography.use-case';
+import { from } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class OpenAiService {}
+export class OpenAiService {
+  checkOrtography(prompt: string) {
+    return from(ortographyUseCase(prompt));
+  }
+}
